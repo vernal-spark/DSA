a6140077856e6f32771eca839528f46017af6c7e
vernal-spark <arshith1234@gmail.com>
2024-10-29 22:54:44 +0530

    Time: 0 ms (100%), Space: 41.2 MB (99.82%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int l=0,h=nums.length-1,mid=-1;
+        while(l<=h){
+            mid=l+(h-l)/2;
+            if(nums[mid]==target){
+                return mid;
+            }
+            if (nums[l] <= nums[mid]) {
+                if (nums[l] <= target && target < nums[mid]) {
+                    h = mid - 1;
+                } else {
+                    l = mid + 1;
+                }
+            } else {
+                if (nums[mid] < target && target <= nums[h]) {
+                    l = mid + 1;
+                } else {
+                    h = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+}
