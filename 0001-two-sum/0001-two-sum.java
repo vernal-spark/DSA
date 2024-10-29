4accf0ad5fc92f133f13d501c58c9558b47ef3df
vernal-spark <arshith1234@gmail.com>
2024-10-29 22:34:45 +0530

    Time: 1 ms (98.96%), Space: 43.5 MB (99.95%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> numToIndex = new HashMap<>();
+        
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (numToIndex.containsKey(complement)) {
+                return new int[] { numToIndex.get(complement), i };
+            }
+            numToIndex.put(nums[i], i);
+        }
+        
+        return null;
+
+        
+    }
+}
