bcec09b4cbd393c252253ebc48481cda7178199d
vernal-spark <arshith1234@gmail.com>
2024-11-02 14:33:12 +0530

    Time: 4 ms (96.1%), Space: 45.2 MB (16.17%) - LeetHub

--- 0155-min-stack/0155-min-stack.java
+++ 0155-min-stack/0155-min-stack.java
@@ -1,34 +1,50 @@
+import java.util.Stack;
+
 class MinStack {
-    private ArrayList<Integer> s;
-    private ArrayList<Integer> minStack;
+    private Stack<Long> s;
+    private long minEle;
 
     public MinStack() {
-        this.s = new ArrayList<>();
-        this.minStack = new ArrayList<>();
+        this.s = new Stack<>();
     }
     
     public void push(int val) {
-        s.add(val);
-        if (minStack.isEmpty() || val <= minStack.get(minStack.size() - 1)) {
-            minStack.add(val);
+        long value = (long) val; 
+        if (s.isEmpty()) {
+            s.push(value);
+            minEle = value;
+        } else {
+            if (value >= minEle) {
+                s.push(value);
+            } else {
+                s.push(2 * value - minEle); 
+                minEle = value;
+            }
         }
     }
     
     public void pop() {
         if (s.isEmpty()) return;
-        int removed = s.remove(s.size() - 1);
-        if (removed == minStack.get(minStack.size() - 1)) {
-            minStack.remove(minStack.size() - 1);
+        
+        long t = s.pop();
+        if (t < minEle) {
+            minEle = 2 * minEle - t; 
         }
     }
     
     public int top() {
         if (s.isEmpty()) return -1;
-        return s.get(s.size() - 1);
+
+        long t = s.peek();
+        if (t < minEle) {
+            return (int) minEle; 
+        } else {
+            return (int) t;
+        }
     }
     
     public int getMin() {
-        if (minStack.isEmpty()) return -1;
-        return minStack.get(minStack.size() - 1);
+        if (s.isEmpty()) return -1; 
+        return (int) minEle;
     }
 }
