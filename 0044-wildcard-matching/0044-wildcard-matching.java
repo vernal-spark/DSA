ff4b3c3c801b1620346af6c858f9b86e37863c12
vernal-spark <arshith1234@gmail.com>
2024-11-05 23:44:20 +0530

    Time: 2 ms (97.62%), Space: 43.7 MB (96.21%) - LeetHub

---
+++ 0044-wildcard-matching/0044-wildcard-matching.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isMatch(final String s, final String p) {
+        int i = 0, j = 0, star = -1, match = -1;
+
+        while(i < s.length()) {
+            if(j < p.length() && (s.charAt(i) == p.charAt(j) || p.charAt(j) == '?')) {
+                i++;
+                j++;
+            } else if(j < p.length() && p.charAt(j) == '*') {
+                star = j++;
+                match = i;
+            } else if(star != -1) {
+                j = star + 1;
+                i = ++match;
+            } else {
+                return false;
+            }
+        }
+
+        while(j < p.length() && p.charAt(j) == '*')
+            j++;
+
+        return i == s.length() && j == p.length();
+    }
+}
