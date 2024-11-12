8bf16791a07e1ac4c6f8c5728e5459054770db38
vernal-spark <arshith1234@gmail.com>
2024-11-12 23:40:18 +0530

    Time: 0 ms (100%), Space: 42.1 MB (30.15%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
@@ -1,25 +1,17 @@
 import java.util.*;
 
 class Solution {
+    public static void right(TreeNode root, int level, List<Integer>ans){
+        if(root==null)return;
+        if(ans.size()==level){
+            ans.add(root.val);
+        }
+        right(root.right,level+1,ans);
+        right(root.left,level+1,ans);
+    }
     public List<Integer> rightSideView(TreeNode root) {
         List<Integer> ans = new ArrayList<>();
-        if (root == null) return ans;
-
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.offer(root);
-
-        while (!queue.isEmpty()) {
-            int size = queue.size();
-            for (int i = 0; i < size; i++) {
-                TreeNode current = queue.poll();
-                if (i == size - 1) {
-                    ans.add(current.val);
-                }
-                if (current.left != null) queue.offer(current.left);
-                if (current.right != null) queue.offer(current.right);
-            }
-        }
-        
+        right(root,0,ans);
         return ans;
     }
 }
